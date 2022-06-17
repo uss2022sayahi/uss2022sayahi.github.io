10505998ee0ee204b8594e7e9efe0257494c824c
Emil Sayahi <97276123+emmyoh@users.noreply.github.com>
2022-06-17 01:24:47 -0400

    One line issue

--- postbuild.sh
+++ postbuild.sh
@@ -2,4 +2,3 @@ sudo npm install -g sass
 /bin/cp -ar ./mokk/assets ./gh-pages
 /bin/cp -ar ./mokk/robots.txt ./gh-pages
 sass ./mokk/assets/css/styles.scss ./gh-pages/assets/css/styles.css
-/bin/cp -ar ./mokk/admin/config.yml ./gh-pages/admin/config.yml
